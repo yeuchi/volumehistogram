54510f28402fb0465863269a5fc74f02a216de73
yeuchi@gmail.com <yeuchi@gmail.com@6a91499e-ffab-11dd-bc25-5f29055a2a2d>
2011-10-13 01:26:51 +0000

    C# test application for DitherLibrary.

---
+++ trunk/TestDitherLib/TestDitherLib/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace TestDitherLib
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnApply = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnApply
+            // 
+            this.btnApply.Location = new System.Drawing.Point(13, 13);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(75, 23);
+            this.btnApply.TabIndex = 0;
+            this.btnApply.Text = "Apply";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 264);
+            this.Controls.Add(this.btnApply);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnApply;
+    }
+}
+
---
+++ trunk/TestDitherLib/TestDitherLib/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestDitherLib
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            Bitmap bmpSrc = (Bitmap)Image.FromFile("testSrc.jpg");
+            Bitmap bmpDes = (Bitmap)((bmpSrc != null) ? new Bitmap(bmpSrc.Width, bmpSrc.Height) : null);
+
+            if (bmpSrc != null)
+            {
+                Dither dither = new Dither();
+                int iRet = dither.invoke(bmpSrc, bmpDes);
+            }
+        }
+    }
+}
